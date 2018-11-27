f4c7956a1eb9be60ac914f221224bcbaa1b259f3
Marcin Wojcik <martins07@wp.pl>
2018-11-27 22:47:28 +0100

    mini project create

---
+++ src/poleProstokata/PoleProstokata.java
@@ -0,0 +1,22 @@
+package poleProstokata;
+
+import java.util.Scanner;
+
+public class PoleProstokata {
+
+    public static void main(String[] args) {
+
+        double a, b;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj dlugosc boku a: ");
+        a = scanner.nextDouble();
+
+        System.out.println("Podaj dlugosc boku b: ");
+        b = scanner.nextDouble();
+
+        double wynik = a * b;
+        System.out.println("Pole prostokata wynosi: " + wynik);
+    }
+
+}
