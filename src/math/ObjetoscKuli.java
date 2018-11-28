fee7f220dda60ce6e30b7afdbd7ea8b05e359e88
Marcin Wojcik <martins07@wp.pl>
2018-11-28 15:53:51 +0100

    pow funcion

--- src/math/ObjetoscKuli.java
+++ src/math/ObjetoscKuli.java
@@ -1,4 +1,17 @@
 package math;
+import java.util.Scanner;
 
 public class ObjetoscKuli {
+
+    public static void main(String[] args) {
+        double r, objetoscKuli;
+        System.out.println("Podaj promien kuli: ");
+        Scanner scanner = new Scanner(System.in);
+        r = scanner.nextDouble();
+
+        //poniesienie do potegi 3stopnia liczby r
+        objetoscKuli = 4 * Math.PI  * Math.pow(r,3)/3;
+        System.out.printf("Objetosc kuli wynosi: " + "%2.2f", objetoscKuli);
+
+    }
 }
