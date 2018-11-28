f91c46f3130ffa4de24188d21cfa87564fcd184b
Marcin Wojcik <martins07@wp.pl>
2018-11-28 20:16:34 +0100

    dzielenie z reszata i bez reszty

---
+++ src/math/BezResztyZdzielenia.java
@@ -0,0 +1,37 @@
+package math;
+
+public class BezResztyZdzielenia {
+
+    int a, b;
+
+    public BezResztyZdzielenia(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void bez() {
+        int result;
+
+        result = a / b;
+        System.out.println("Wynik bez reszty: " + result);
+    }
+
+    public void reszta() {
+        int result;
+
+        result = a % b;
+        System.out.println("Wynik z reszta: " + result);
+    }
+
+    public static void main(String[] args) {
+
+        BezResztyZdzielenia bezReszty = new BezResztyZdzielenia(37,11);
+        BezResztyZdzielenia zReszta = new BezResztyZdzielenia(37,11);
+
+        bezReszty.bez();
+        bezReszty.reszta();
+
+
+    }
+
+}
