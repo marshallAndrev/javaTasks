ac46f5570dd8ef200b389737fdc5b77f4f8121d3
Marcin Wojcik <martins07@wp.pl>
2018-11-28 15:13:20 +0100

    sqrt for pi number

--- src/math/NumberPiPrint.java
+++ src/math/NumberPiPrint.java
@@ -7,6 +7,9 @@ public class NumberPiPrint {
         //printf change print to format print(speclial metod)
         //%6.5f means we reserve space to print 6 (1 is ineger 5 are after .
         System.out.printf("PI = " + "%6.5f", Math.PI);
+        
+        //sqrt from pi
+        System.out.printf("\n" + "Pierwiastek kwadratowy: " + "%2.2f", Math.sqrt(Math.PI));
 
     }
 }
